a3d1a119a3ba1590f9f2cd3610d0ae8a9addbeac
sheshanth7 <work.sheshanth@outlook.com>
2025-07-05 22:54:47 +0530

    initial commit

---
+++ src/test/java/com/learning/currency_convertor/CurrencyConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.currency_convertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
